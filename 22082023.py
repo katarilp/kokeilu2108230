b2bf1cc91f307fa0f094fd99c6bbea177d7d0243
katarilp <katarilp@metropolia.fi>
2023-08-22 13:44:27 +0300

    tehtävät 1 ja 2

--- 22082023.py
+++ 22082023.py
@@ -1,1 +1,10 @@
-print('hei')
+import math
+#tehtävä 1
+name = input("Hei, mikä on nimesi? ")
+print("Hauska tavata " +name +"!")
+
+#tehtävä 2
+r=int(input("Syötä ympyrän säde, niin kerron sen pinta-alan. "))
+A = (r * r) *3.14
+print(A)
+
