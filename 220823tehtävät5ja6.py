782e6b5f39c0e48ca673cf2f60e1ce2d9f7ed7f5
katarilp <katarilp@metropolia.fi>
2023-08-22 15:29:13 +0300

    tehtävät 3 ja 4

---
+++ 220823tehtävät5ja6.py
@@ -0,0 +1,18 @@
+luo = 13.3
+nau = 32 * 13.3
+lei = 640 * 13.3
+num1 = float(input("Anna paino leivisköinä. "))
+num2 = float(input("Anna paino nauloina. "))
+num3 = float(input("Anna paino luoteina. "))
+num4 = num1 * lei
+num5 = num2 * nau
+num6 = num3 * 13.3
+mas = num4 + num5 + num6
+kil = str(mas // 1000)
+gra = mas % 1000
+gr = str(round(gra,2))
+print("Paino on moderneilla yksiköillä " +kil+ "kilogrammaa ja " +gr+ "grammaa.")
+
+tehtävä 6
+
+
