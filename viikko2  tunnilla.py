e03122e58e9fa3788327b1e083ba1fbbf9be6520
katarilp <katarilp@metropolia.fi>
2023-08-28 15:51:16 +0300

    28.8.2023

--- viikko2  tunnilla.py
+++ viikko2  tunnilla.py
@@ -1,3 +1,37 @@
 print ("Huomenta!")
+import math
+
+luku = 3.5
+print(math.floor(luku))
+print(math.ceil(luku))
+print(round(luku))
+print(int(luku))
+
+rahat = float(input("Anna rahamäärä: "))
+#ehtolause
+if rahat >= 5:
+    #lohko
+    print("Osta latte, ole hyvä.")
+if rahat < 5:
+    print("Mene kotiisi.")
+
+#
+A = True
+B = False
+#
+print(4<69)
+
+lauta = int(input("Anna laudan pituus: "))
+if 300 > lauta >= 200 :
+    print("Maalaa lauta punaiseksi.")
+else :
+    print("Kokeile toista lautaa.")
 
 
+num = int(input("Anna numero: "))
+if num < 0 :
+    print("Numero on negatiivinen.")
+if num == 0 :
+    print("Numerosi on nolla.")
+if num > 0 :
+    print("Numerosi on positiivinen.")
