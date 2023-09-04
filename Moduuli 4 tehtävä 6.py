da3705e6559a70f09d3e54dbf098cab99d8707c2
katarilp <katarilp@metropolia.fi>
2023-09-04 15:24:12 +0300

    andrei

--- Moduuli 4 tehtävä 6.py
+++ Moduuli 4 tehtävä 6.py
@@ -1,9 +1,16 @@
 import random
 # x ** 2 + y ** 2 < 1
 
-N = float(input("Syötä pisteiden määrä: "))
-x = 0
-pisteet = random.uniform (-1, 1)
-while x < N:
-    
-    x += 1
+N = int(input("Syötä pisteiden määrä: "))
+n = kierros = y = x = 0
+# x ja y arvoja ei välttämättä tarvitse tässä vaiheessa määrittää
+
+while kierros < N:
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+#   print(f"x: {x} ja y: {y}") jos kokeilee pienillä määrällä
+    kierros += 1
+    if x ** 2 + y ** 2 < 1: #== True:
+        n += 1
+
+print(4 * n / N )
