e61925cbe67aec1d1be833633f33fb9ff9d8f9ab
katarilp <katarilp@metropolia.fi>
2023-09-04 14:06:54 +0300

    käyty tunnilla

--- Moduuli 3 tehtävä 4.py
+++ Moduuli 3 tehtävä 4.py
@@ -1,4 +1,6 @@
 year = int(input("Anna vuosiluku: "))
+
+# if year % 4 == 0 and year % 400 == 0 or year % 4 == 0 and year % 100 != 0: olisi sama yhdellä rivillä
 if year % 100 == 0 and year % 400 == 0:
     print("Antamasi vuosi on karkausvuosi.")
 elif year % 4 == 0 and year % 100 != 0:
