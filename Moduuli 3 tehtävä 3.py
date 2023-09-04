b414a54b622111707c78fa0da36fe012f9d92eeb
katarilp <katarilp@metropolia.fi>
2023-09-04 13:48:30 +0300

    käyty tunnilla

--- Moduuli 3 tehtävä 3.py
+++ Moduuli 3 tehtävä 3.py
@@ -1,14 +1,17 @@
 sex = input("Kerro biologinen sukupuolesi: ")
-sex1 = (sex.lower())
+sex = sex.lower()
 hg = int(input("Kerro hemoglobiiniarvosi: "))
-if sex1 == "mies":
+if sex != "mies" and sex != "nainen":
+    print("Virheellinen sukupuoli.")
+    quit()
+if sex == "mies":
     if hg < 134:
         print("Hemoglobiiniarvosi on alhainen.")
     elif 134 <= hg <= 195:
         print("Hemoglobiiniarvosi on normaali.")
     elif hg > 195:
         print("Hemoglobiiniarvosi on korkea.")
-elif sex1 == "nainen":
+elif sex == "nainen":
     if hg < 117:
         print("Hemoglobiiniarvosi on alhainen.")
     elif 117 <= hg <= 175:
