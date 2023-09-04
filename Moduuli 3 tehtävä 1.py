83a942f0660bef85c13bc20b904d7dc23eff6f92
katarilp <katarilp@metropolia.fi>
2023-09-04 13:24:28 +0300

    käyty tunnilla

--- Moduuli 3 tehtävä 1.py
+++ Moduuli 3 tehtävä 1.py
@@ -1,7 +1,6 @@
-#Kuha on alamittainen, jos se on alle 37 cm
 mitta = int(input("Anna kuhan pituus: "))
-if  mitta >= 37:
-    print ("Onnittelut pyyntimittaisesta kuhasta!")
+if mitta >= 37:
+    print("Onnittelut pyyntimittaisesta kuhasta!")
 elif mitta < 37:
     print("Kuha on alamittainen. Laske se takaisin järveen.")
     ero = str(37 - mitta)
