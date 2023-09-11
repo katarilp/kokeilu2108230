3e66e719e541a3d1e4e4ae854372f602fcb898bb
katarilp <katarilp@metropolia.fi>
2023-09-11 14:32:06 +0300

    5.9.2023

--- Moduuli 5 tehtävä 4.py
+++ Moduuli 5 tehtävä 4.py
@@ -4,10 +4,10 @@
 #käytä for-toistorakennetta nimien kysymiseen ja for/in toistorakennetta niiden läpikäymiseen.
 
 stad = []
-i = 0
+
 for i in range(5):
     stad.append(input("Syötä kaupungin nimi: "))
-    i += 1
+
 print("Syöttämäsi kaupungit:")
 for i in range(5):
     print(f"{stad[i]}")
