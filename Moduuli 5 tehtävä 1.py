8a36f70e13a4ec2e1b25e9c0d744b6b0f4f2032d
katarilp <katarilp@metropolia.fi>
2023-09-11 13:26:14 +0300

    5.9.2023

--- Moduuli 5 tehtävä 1.py
+++ Moduuli 5 tehtävä 1.py
@@ -3,10 +3,10 @@
 # Käytä for-toistorakennetta.
 import random
 nopat = int(input("Kerro noppien lukumäärä: "))
-i = 0
 heitto = []
+
 for i in range(nopat):
-    heitto.append(random.randint(1,7))
-    i += 1
+    heitto.append(random.randint(1,6))
+
 summa = sum(heitto)
 print(f"Noppien silmälukujen summa on {summa}.")
