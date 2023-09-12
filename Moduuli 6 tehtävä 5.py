eaa8e1e56dde5e802a92d0fa1db5c33b4c1da650
katarilp <katarilp@metropolia.fi>
2023-09-12 14:25:00 +0300

    5.9.2023

--- Moduuli 6 tehtävä 5.py
+++ Moduuli 6 tehtävä 5.py
@@ -2,3 +2,21 @@
 # Ohjelma palauttaa toisen listan, joka on muuten samanlainen kuin parametrina saatu lista paitsi että siitä on
 # karsittu pois kaikki parittomat luvut. Kirjoita testausta varten pääohjelma, jossa luot listan, kutsut funktiota
 # ja tulostat sen jälkeen sekä alkuperäisen että karsitun listan.
+def karsinta(luvut):
+    parilliset = []
+    lista = luvut.copy()
+    for i in lista:
+        if i % 2 == 0:
+            parilliset.append(i)
+    return parilliset
+
+luvut = [23, 2, 93, 14, 8, 97, 11, 3, 7, 66, 31, 5]
+parilliset_lista = karsinta(luvut)
+print("Listan kaikki luvut: ")
+print(luvut)
+print("Listan parilliset luvut:")
+print(parilliset_lista)
+
+
+
+
