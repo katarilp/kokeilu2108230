ba4efa0f40c9f4d2f078a8a8973b812f0c5e46d4
katarilp <katarilp@metropolia.fi>
2023-09-12 13:50:14 +0300

    5.9.2023

--- Moduuli 6 tehtävä 4.py
+++ Moduuli 6 tehtävä 4.py
@@ -1,3 +1,13 @@
 # Kirjoita funktio, joka saa parametrinaan listan kokonaislukuja.
 # Ohjelma palauttaa listassa olevien lukujen summan.
 # Kirjoita testausta varten pääohjelma, jossa luot listan, kutsut funktiota ja tulostat sen palauttaman summan.
+def summa(luvut):
+    print("Lasketaan listan luvut yhteen: ")
+    tulos = 0
+    for i in luvut:
+        tulos = tulos + i
+    return tulos
+luvut = [10, 20, 30, 40, 50]
+yhteenlaskettu = summa(luvut)
+
+print(yhteenlaskettu)
