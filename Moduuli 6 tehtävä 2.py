0ea8b685d4ec7411e4916767acf4f602400ddbee
katarilp <katarilp@metropolia.fi>
2023-09-12 12:55:40 +0300

    5.9.2023

--- Moduuli 6 tehtävä 2.py
+++ Moduuli 6 tehtävä 2.py
@@ -2,3 +2,13 @@
 # Muokatun funktion avulla voit heitellä esimerkiksi 21-tahkoista roolipelinoppaa.
 # Edellisestä tehtävästä poiketen nopan heittelyä jatketaan pääohjelmassa kunnes saadaan nopan maksimisilmäluku,
 # joka kysytään käyttäjältä ohjelman suorituksen alussa.
+
+import random
+tahkot= int(input("Nopan tahkot: "))
+def tulos():
+    return random.randint(1,tahkot)
+silmäluku = tulos()
+while silmäluku != tahkot:
+    print(silmäluku)
+    silmäluku = tulos()
+print(silmäluku)
