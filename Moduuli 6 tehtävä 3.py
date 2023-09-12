d9301bc76a536b225886d4afb97e76a9199f7833
katarilp <katarilp@metropolia.fi>
2023-09-12 13:19:58 +0300

    5.9.2023

--- Moduuli 6 tehtävä 3.py
+++ Moduuli 6 tehtävä 3.py
@@ -3,3 +3,14 @@
 # Muunnos on tehtävä aliohjelmaa hyödyntäen. Muuntamista jatketaan siihen saakka, kunnes käyttäjä syöttää
 # negatiivisen gallonamäärän.
 # Yksi gallona on 3,785 litraa.
+
+def muunnos(num):
+    tulos = num * 3.785
+    return tulos
+
+num = float(input("Anna bensiinin määrä nestegallonoina: "))
+while num >= 0:
+    litrat = muunnos(num)
+    print("Bensiinin määrä litroina:")
+    print(litrat)
+    num = float(input("Anna bensiinin määrä nestegallonoina: "))
