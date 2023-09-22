62f91d6364f766d6be4b70916a7426427a0897f0
katarilp <katarilp@metropolia.fi>
2023-09-22 10:43:47 +0300

    21.9.2023

--- Moduuli 8 tehtävä 2.py
+++ Moduuli 8 tehtävä 2.py
@@ -1,3 +1,18 @@
 # Kirjoita ohjelma, joka kysyy käyttäjältä maakoodin (esimerkiksi FI) ja tulostaa kyseisessä maassa olevien
 # lentokenttien lukumäärät tyypeittäin. Esimerkiksi Suomen osalta tuloksena on saatavatieto siitä,
 # että pieniä lentokenttiä on 65 kappaletta, helikopterikenttiä on 15 kappaletta jne.
+
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='Kata',
+    password='password',
+    autocommit=True
+)
+cursor = connection.cursor()
+
+# maa = input("Anna hakemasi maakoodi: ")
+# maa.upper()
--- Moduuli 8 tehtävä 3.py
+++ Moduuli 8 tehtävä 3.py
@@ -2,8 +2,6 @@
 # Ohjelma ilmoittaa lentokenttien välisen etäisyyden kilometreinä.
 # Laskenta perustuu tietokannasta haettuihin koordinaatteihin.
 # Laske etäisyys geopy-kirjaston avulla: https://geopy.readthedocs.io/en/stable/.
-# Asenna kirjasto valitsemalla View / Tool Windows / Python Packages.
-# Kirjoita hakukenttään geopy ja vie asennus loppuun.
+# koordinaatit tuple-muotoon tietokannasta, latitude_deg ja longitude_deg airportista where ident= haettu
 
 from geopy import distance
-koordinaatit tuple-muotoon tietokannasta, latitude_deg ja longitude_deg airportista where ident= haettu
